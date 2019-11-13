16060b631d535f61f01de011df31dfac83c4922a
lucas poirel <lucas.poirel@epitech.eu>
2019-11-13 16:13:22 +0100

    splashscreen

--- client/src/core/SplashScreen/SplashScreen.cpp
+++ client/src/core/SplashScreen/SplashScreen.cpp
@@ -7,16 +7,18 @@
 RType::SplashScreen::SplashScreen(sf::RenderWindow *app, WindowState *state) : _app(app), _windowState(state) {
     this->_currentTexture = new sf::Texture();
     this->_currentTexture->loadFromFile("assets/SplashScreen/background.png");
-    this->_lastFrame = 300;
+    this->_lastFrame = 100000;
     this->_currentFrame = 0;
     this->_done = false;
     this->_splash.setTexture(*this->_currentTexture);
 }
 
 void RType::SplashScreen::run() {
-    if (this->_currentFrame == this->_lastFrame)
+    if (this->_currentFrame == this->_lastFrame || this->_clock.getElapsedTime().asSeconds() == 3)
         this->_done = true;
-    if (!this->_done) {
+        this->_windowState->setSplashDone(true);
+        this->_windowState->setIsLoading(true);
+    if (!this->_done || this->_clock.getElapsedTime().asSeconds() < 3) {
         this->_currentFrame += 1;
         _app->clear();
         _app->draw(_splash);
