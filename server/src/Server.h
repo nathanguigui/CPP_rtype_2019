badb0390205726dfa3dff852edc03f3f81e3d0bc
Lcomte <lucas.comte@epitech.eu>
2019-11-13 14:15:38 -0800

    need merge for CMakelist

---
+++ server/src/Server.h
@@ -0,0 +1,14 @@
+//
+// Created by Lucas on 11/13/2019.
+//
+
+#ifndef CPP_RTYPE_2019_SERVER_H
+#define CPP_RTYPE_2019_SERVER_H
+
+
+class Server {
+
+};
+
+
+#endif //CPP_RTYPE_2019_SERVER_H
---
+++ server/src/ServerManager.h
@@ -0,0 +1,14 @@
+//
+// Created by Lucas on 11/13/2019.
+//
+
+#ifndef CPP_RTYPE_2019_SERVERMANAGER_H
+#define CPP_RTYPE_2019_SERVERMANAGER_H
+
+
+class ServerManager {
+
+};
+
+
+#endif //CPP_RTYPE_2019_SERVERMANAGER_H
