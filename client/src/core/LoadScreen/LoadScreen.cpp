4916698aa007de279b93c54602db1371449ddf5a
lucas poirel <lucas.poirel@epitech.eu>
2019-11-29 16:09:19 +0100

    add LoadScreen

---
+++ client/src/core/LoadScreen/LoadScreen.cpp
@@ -0,0 +1,57 @@
+//
+// Created by guigui on 11/13/19.
+//
+
+#include <client/src/core/Gui/Tools.hpp>
+#include "LoadScreen.hpp"
+
+RType::LoadScreen::LoadScreen(sf::RenderWindow *app, WindowState *state) : _app(app), _windowState(state) {
+    this->_splashTexture = new sf::Texture();
+    this->_backgroundTexture = new sf::Texture();
+    this->_splash = new sf::Sprite();
+    this->_background = new sf::Sprite();
+    this->_splashTexture->loadFromFile("assets/bullets/r-typesheet1.gif");
+    this->_backgroundTexture->loadFromFile("assets/MainMenu/Background.png");
+
+    this->_background->setTexture(*this->_backgroundTexture);
+    this->_splash->setTexture(*this->_splashTexture);
+
+    this->boum = new SimpleExplosion();
+    this->kill = new SimpleKill();
+    this->touch = new SuperTouch();
+    this->simpleTouch = new SimpleTouch();
+
+    this->redenemy = new LittleRed();
+
+    this->bullet = new SimpleBullet();
+    this->master = new MasterBullet();
+    this->super1 = new SuperBullet1();
+    this->super2 = new SuperBullet2();
+    this->super3 = new SuperBullet3();
+    this->bomb = new SimpleBomb();
+
+    //this->_splash->setTextureRect(*this->_rectSourceSprite);
+
+    //this->_buffer.loadFromFile("assets/music/splashscreen.wav");    
+    //this->_sound.setBuffer(this->_buffer);
+    //this->_sound.play();
+}
+
+void RType::LoadScreen::run() {
+
+    auto screenSize = this->_app->getSize();
+    this->_splash->setPosition(centerX(screenSize, this->_splash->getGlobalBounds().width, this->_splash->getPosition()));
+    this->_splash->setPosition(centerY(screenSize, this->_splash->getGlobalBounds().height, this->_splash->getPosition()));
+
+    this->_splash->setTextureRect(this->boum->run());
+
+    if (this->_splashClock->getElapsedTime().asSeconds() > 0.05f)
+    {
+        this->_splash->setTextureRect(this->boum->run());
+        this->_splashClock->restart();
+    }
+    _app->draw(*this->_background);
+    _app->draw(*this->_splash);
+}
+
+RType::LoadScreen::~LoadScreen() = default;
---
+++ client/src/core/LoadScreen/LoadScreen.hpp
@@ -0,0 +1,69 @@
+//
+// Created by guigui on 11/13/19.
+//
+
+#ifndef CPP_RTYPE_2019_LOADSCREEN_HPP
+#define CPP_RTYPE_2019_LOADSCREEN_HPP
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include "client/src/core/WindowState/WindowState.hpp"
+#include "client/src/core/CoreObject/CoreObject.hpp"
+
+#include "../../game/Bullets/BulletsShoot/SimpleBullet/SimpleBullet.h"
+#include "../../game/Bullets/BulletsShoot/SuperBullet1/SuperBullet1.h"
+#include "../../game/Bullets/BulletsShoot/SuperBullet2/SuperBullet2.h"
+#include "../../game/Bullets/BulletsShoot/SuperBullet3/SuperBullet3.h"
+#include "../../game/Bullets/BulletsShoot/MasterBullet/MasterBullet.h"
+#include "../../game/Bullets/BulletsShoot/SimpleBomb/SimpleBomb.h"
+
+#include "../../game/Bullets/BulletTouch/SimpleKill/SimpleKill.h"
+#include "../../game/Bullets/BulletTouch/SimpleExplosion/SimpleExplosion.h"
+#include "../../game/Bullets/BulletTouch/SuperTouch/SuperTouch.h"
+#include "../../game/Bullets/BulletTouch/SimpleTouch/SimpleTouch.h"
+
+#include "../../game/enemies/LittleRed/LittleRed.h"
+
+namespace RType {
+    using namespace RType;
+    class LoadScreen: public CoreObject {
+    public:
+        LoadScreen(sf::RenderWindow *app, WindowState *state);
+        void run();
+        ~LoadScreen();
+
+
+    private:
+        RType::SuperBullet1 *super1;
+        RType::SuperBullet2 *super2;
+        RType::SuperBullet3 *super3;
+        RType::SimpleBullet *bullet;
+        RType::MasterBullet *master;
+        RType::SimpleBomb *bomb;
+
+        RType::SimpleKill *kill;
+        RType::SimpleExplosion *boum;
+        RType::SuperTouch *touch;
+        RType::SimpleTouch *simpleTouch;
+
+        RType::LittleRed *redenemy;
+
+        sf::SoundBuffer _buffer;
+        sf::Sound _sound;
+
+        sf::Sprite *_splash;
+        sf::Sprite *_background;
+        sf::IntRect *_rectSourceSprite;
+        sf::Texture *_splashTexture;
+        sf::Texture *_backgroundTexture;
+        sf::Clock *_clock;
+        sf::Clock *_splashClock;
+        sf::RenderWindow *_app;
+        bool _done;
+        int _currentFrame;
+        int _lastFrame;
+        WindowState *_windowState;
+    };
+}
+
+
+#endif //CPP_RTYPE_2019_SPLASHSCREEN_HPP
